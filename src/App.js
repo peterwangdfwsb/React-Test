077065d174752c4febc2693298e523f2029389c2
peterwangdfwsb <68233045+peterwangdfwsb@users.noreply.github.com>
2022-06-05 17:52:38 -0400

    Test from https://medium.com/hannah-lin/jest-enzyme-1-%E5%B0%B1%E5%BE%9E%E8%A8%88%E6%95%B8%E5%99%A8%E9%96%8B%E5%A7%8B-bd4d7d223f72

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import "./styles.css";
+import React, { Component } from "react";
+//import "./styles.scss";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+  render() {
+    return (
+      <div id="component-app">
+        <div id="counter-display">
+          <h1>{this.state.counter}</h1>
+        </div>
+        <button
+          id="increment-button"
+          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+        >
+          Add
+        </button>
+      </div>
+    );
+  }
 }
+
+export default App;
